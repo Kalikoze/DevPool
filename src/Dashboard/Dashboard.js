a440f83b8957d96c60e16cbbcc4a1ca8e4626def
Travis Rollins <kalikoze@gmail.com>
2017-12-15 12:15:01 -0700

    Create welcome component for dashboard.

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Tabs from '../Tabs/Tabs';
+import Welcome from '../Welcome/Welcome';
 
 export default class Dashboard extends Component {
   render() {
     return (
       <section className="dashboard">
+        <Welcome />
         <Tabs />
       </section>
     )
---
+++ src/Welcome/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Welcome = () => {
+  return (
+    <section className='welcome'>
+      <h2>Welcome Back Travis!</h2>
+      <p>Here's What's New...</p>
+    </section>
+  )
+};
+
+export default Welcome
