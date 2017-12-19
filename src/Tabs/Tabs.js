9498335f5e9a7ffa1e807434229ee241ea85622c
Travis Rollins <kalikoze@gmail.com>
2017-12-18 18:07:38 -0700

    Create tab component and style it.

--- src/Tabs/Tabs.js
+++ src/Tabs/Tabs.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Tab from '../Tab/Tab';
 
 const Tabs = () => {
   return (
     <section className='tabs'>
-      
+      <Tab text="Overview"/>
+      <Tab text="Job Activity"/>
+      <Tab text="Messages"/>
+      <Tab text="Calendar"/>
+      <Tab text="Account Settings"/>
     </section>
   )
 }
